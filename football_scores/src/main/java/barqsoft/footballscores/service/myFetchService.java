c30685f06ef333c98e5da0d7776880dd98b3f797
easycheese <kyle.smaagard@gmail.com>
2015-08-23 22:16:38 -0400

    Updated widget info

--- football_scores/src/main/java/barqsoft/footballscores/service/myFetchService.java
+++ football_scores/src/main/java/barqsoft/footballscores/service/myFetchService.java
@@ -1,11 +1,14 @@
 package barqsoft.footballscores.service;
 
 import android.app.IntentService;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -19,11 +22,13 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 import java.util.TimeZone;
 import java.util.Vector;
 
 import barqsoft.footballscores.content.DatabaseContract;
 import barqsoft.footballscores.R;
+import barqsoft.footballscores.widget.AppWidgetProvider;
 
 /**
  * Created by yehya khaled on 3/2/2015.
@@ -41,7 +46,7 @@ public class myFetchService extends IntentService
     {
         getData("n2");
         getData("p2");
-
+        updateWidgets();
         return;
     }
 
@@ -131,6 +136,18 @@ public class myFetchService extends IntentService
         catch(Exception e)
         {
             Log.e(LOG_TAG,e.getMessage());
+        }
+
+    }
+    private void updateWidgets() {
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this
+                .getApplicationContext());
+        int[] ids = appWidgetManager.getAppWidgetIds(new ComponentName(getApplicationContext(), AppWidgetProvider.class));
+        for(int widgetId:ids) {
+            RemoteViews views = new RemoteViews(getApplicationContext().getPackageName(), R.layout.widget_layout);
+            int number = (new Random().nextInt(10000));
+            views.setTextViewText(R.id.score, number + "");
+            appWidgetManager.updateAppWidget(widgetId,views);
         }
     }
     private void processJSONdata (String JSONdata,Context mContext, boolean isReal)
--- football_scores/src/main/java/barqsoft/footballscores/widget/AppWidgetProvider.java
+++ football_scores/src/main/java/barqsoft/footballscores/widget/AppWidgetProvider.java
@@ -4,7 +4,12 @@ import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.RemoteViews;
 
+import java.util.Calendar;
+import java.util.Random;
+
+import barqsoft.footballscores.R;
 import barqsoft.footballscores.activity.MainActivity;
 
 /**
@@ -15,21 +20,36 @@ public class AppWidgetProvider extends android.appwidget.AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         final int N = appWidgetIds.length;
 
-        // Perform this loop procedure for each App Widget that belongs to this provider
         for (int i=0; i<N; i++) {
             int appWidgetId = appWidgetIds[i];
+            int number = (new Random().nextInt(100));
 
-            // Create an Intent to launch ExampleActivity
             Intent intent = new Intent(context, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
-            // Get the layout for the App Widget and attach an on-click listener
-            // to the button
-//            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.appwidget_provider_layout);
+            RemoteViews view = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+
+            view.setTextViewText(R.id.score, number + "");
 //            views.setOnClickPendingIntent(R.id.button, pendingIntent);
 
+
+//            // Register an onClickListener
+//            Intent intent = new Intent(context, AppWidgetProvider.class);
+//
+//            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+//            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+//
+//            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+//                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            view.setOnClickPendingIntent(R.id.score, pendingIntent);
+
             // Tell the AppWidgetManager to perform an update on the current app widget
-//            appWidgetManager.updateAppWidget(appWidgetId, views);
+            appWidgetManager.updateAppWidget(appWidgetId, view);
         }
+    }
+    public static void updateWidget(AppWidgetManager appWidgetManager, RemoteViews view, int appWidgetId) { //TODO combine data from fetchService
+
+
+        appWidgetManager.updateAppWidget(appWidgetId, view);
     }
 }
